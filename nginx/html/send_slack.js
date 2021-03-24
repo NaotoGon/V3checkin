77a4625054bddc8ac01eafe24591fad354fd58be
NaotoGon <naotogon5710@gmail.com>
2021-03-24 15:06:40 +0900

    リンク表示

--- nginx/html/send_slack.js
+++ nginx/html/send_slack.js
@@ -1,6 +1,7 @@
 window.onload = function(){
   
   post_slack("generating zoom link...");
+  getJsonp_GAS();
 
   function post_slack(message) {
     const url = 'https://hooks.slack.com/services/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';
@@ -11,6 +12,19 @@ window.onload = function(){
         type: 'POST',
         url:  url,
         data: `payload=${JSON.stringify(data)}`
+    });
+  }
+  function getJsonp_GAS() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://script.google.com/macros/s/AKfycbz_lkmIjo4HwsHKATiCmjwRCTf18Xoa9DBFrIDnS7OvsRhlxRjBrZum7QDrZNWmFMJtIg/exec',
+      dataType: 'jsonp',
+      jsonpCallback: 'jsondata',
+      success: function(json){
+      var len = json.length;
+      var html = json[len-1].url;
+    document.getElementById('zoomlink').innerHTML = html;
+    }
     });
   }
 }
